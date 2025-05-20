c0e026d256966c1da3eea604fe92d554bb77d22b
lalitanar <lalita.nar@mahidol.edu>
2025-05-20 12:01:26 +0700

    ex07-cardView

---
+++ ex07_cardview/app/src/main/java/com/egci428/ex07_cardview/Adapter/MovieAdapter.kt
@@ -0,0 +1,8 @@
+package com.egci428.ex07_cardview.Adapter
+
+/*
+* Created by Lalita N. on 20/5/25
+*/
+
+class MovieAdapter {
+}
---
+++ ex07_cardview/app/src/main/java/com/egci428/ex07_cardview/Model/Movie.kt
@@ -0,0 +1,7 @@
+package com.egci428.ex07_cardview.Model
+
+/*
+* Created by Lalita N. on 20/5/25
+*/
+
+data class Movie()
