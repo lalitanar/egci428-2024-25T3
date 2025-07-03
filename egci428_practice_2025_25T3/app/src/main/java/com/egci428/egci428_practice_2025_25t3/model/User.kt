50a4c638f29e0cb01f639ee5ad6f0fde59c77d9c
lalitanar <lalita.nar@mahidol.edu>
2025-07-03 13:40:45 +0700

    practice

---
+++ egci428_practice_2025_25T3/app/src/main/java/com/egci428/egci428_practice_2025_25t3/model/User.kt
@@ -0,0 +1,2 @@
+package com.egci428.egci428_practice_2025_25t3.model 
+
---
+++ inclass11_firestoreRating/app/src/main/java/com/egci428/inclass11_firestorerating/Message.kt
@@ -0,0 +1,8 @@
+package com.egci428.inclass11_firestorerating
+
+/*
+* Created by Lalita N. on 27/6/25
+*/
+
+class Message {
+}
